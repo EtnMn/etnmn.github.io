39c1a23ff989f602fc59a79e20f7833830c369c7
Etienne <etienne@menou.fr>
2025-09-10 21:47:35 +0000

    feat: update packages (#15)
    * chore: update devcontainer pnpm
    
    * refactor: update to tailwindcss v4
    
    * feat: add AZ-104 certification

--- front/astro.config.mjs
+++ front/astro.config.mjs
@@ -1,15 +1,19 @@
 import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://menou.fr",
-    integrations: [tailwind(), icon({ iconDir: "src/assets/icons" })],
-    markdown: {
-        shikiConfig: {
-            theme: "dracula",
-        },
-    },
+  site: "https://menou.fr",
+  integrations: [icon({ iconDir: "src/assets/icons" })],
+  markdown: {
+      shikiConfig: {
+          theme: "dracula",
+      },
+  },
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
